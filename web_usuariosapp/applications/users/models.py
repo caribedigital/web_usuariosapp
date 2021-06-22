48878dc5d9e3cea1dde1893ca5fc7477c8e5c3d7
caribedigital <ilse.alejo@gmail.com>
2021-06-21 23:10:04 -0400

    actualizacion 21jun2021

--- web_usuariosapp/applications/users/models.py
+++ web_usuariosapp/applications/users/models.py
@@ -14,22 +14,22 @@ class User(AbstractBaseUser, PermissionsMixin): #AbstractBaseUser por defecto co
         ('O', 'Otros'),
     )
 
-    username = models.CharField(max_length=15, unique=True)
+    username = models.CharField(max_length=15, unique=True) #unique no permite que se creen usuarios iguales
     email = models.EmailField()
     nombres = models.CharField(max_length=30, blank=True) #blank=True este atributo no es obligatorio
     apellidos = models.CharField(max_length=30, blank=True)
-    genero = models.CharField(max_length=1, choices=GENDER_CHOICES, blank=True)
+    genero = models.CharField(max_length=1, choices=GENDER_CHOICES, blank=True)#cuando tenemos un argumento choices, debemos crear las opciones arriba de las variables del modelo
     #atributo booleano para que el usuario creado pueda acceder al administrador
     is_staff = models.BooleanField(default=False) #un usuario normal no puede acceder al admin, porque por defecto el campo o atributo is_staff esta en False
                                                   #aunque desde el superusuario al ingresar en la app users en el admin, e ingresar en un usuario, este tiene campo is_staff pero desactivado, y tildando desde superuser, se le pueden dar pleno acceso al admin.
-    USERNAME_FIELD = 'username'
+    USERNAME_FIELD = 'username'#establece con cual atributo se van loguear los usuarios
 
     REQUIRED_FIELDS = ['email',]
 
     objects = UserManager() #conecta el manager con el modelo
 
-    def get_short_name(self):
+    def get_short_name(self):#funcion nombre corto
         return self.username
 
-    def get_full_name(self):
+    def get_full_name(self):#funcion nombre completo
         return self.nombres + ' ' + self.apellidos
